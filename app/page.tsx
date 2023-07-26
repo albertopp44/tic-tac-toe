9bc9a76d3fa8c4bbe00a6089220db94a69e0ab4b
albertopp44 <99272027+albertopp44@users.noreply.github.com>
2023-07-26 16:37:31 +0200

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
 //what our users are going to play the game
 
 // with this snippet from pages\index.tsx:
-
+'use client'
 
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import Board from '../components/Board';
-import { calculateWinner } from '../helpers/calculateWinner.ts';
+import { calculateWinner } from '../helpers/calculateWinner';
+
+//now using useState hook and useEffect hook we have to set the state of the game, frist player, then the next player, then the winner
 
 export default function Home() {
-  return ( )
+  const [board, setBoard] = useState<string[]>(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [winner, setWinner] = useState<string | null>(null);
+
+  return ( 
+    <div className="container">
+      <h1>Tic Tac Toe</h1>
+      <Board squares={board} onClick={handleClick} />
+      <div className="info-wrapper">
+        <div>
+          <h3>History</h3>
+          {/* <History history={history} moveTo={moveTo} currentMove={stepNumber} /> */}
+        </div>
+        <h3>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</h3>
+      </div>
+    </div>
+  );
+
 }
